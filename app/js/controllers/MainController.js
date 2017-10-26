8b971705b2de50e1a14b5f3a6fd3598b5fa2a7b2
dgsousa <sousa.danielg@gmail.com>
2017-10-25 19:22:13 -0700

    adjustments

--- app/js/controllers/MainController.js
+++ app/js/controllers/MainController.js
@@ -4,16 +4,17 @@ function MainController($scope, $timeout, wikidata) {
 
 	const vm = this;
 
-	vm.enter = function(e) { 
+	vm.enter = (e) => { 
 		if(e.which === 13) {
 			vm.results = [];
-			$timeout(function() {
+			$timeout(() => {
 				wikidata.getWikis($scope.search, getData);
 			}, 300);  
 		}
 	};
 
-	const getData = function(response) {
+	const getData = (response) => {
+
 		if(response.status = 200) {
 			vm.results = response.data.query.pages; 
 		} 
--- app/js/services/wikidata.js
+++ app/js/services/wikidata.js
@@ -9,8 +9,8 @@ const wikidata = ($http, $log) => {
 
 	return {
 		
-		getWikis: function(string, callback) {
-			$http.jsonp("https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&list=&titles=&generator=search&exsentences=1&exlimit=10&exintro=1&explaintext=1&gsrsearch=" + string)
+		getWikis: function(query, callback) {
+			$http.jsonp("https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&list=&titles=&generator=search&exsentences=1&exlimit=10&exintro=1&explaintext=1&gsrsearch=" + query)
 				.then(callback)
 				.catch(logError);
 		}	
