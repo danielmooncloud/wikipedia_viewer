8551b759775dea2bfec9f98c32a2d198562a5a23
dgsousa <sousa.danielg@gmail.com>
2018-01-27 12:42:19 -0800

    added errorhandling

--- app/js/services/wikidata.js
+++ app/js/services/wikidata.js
@@ -4,7 +4,7 @@ const wikidata = ($http, $log) => {
 	
 	return {
 		getWikis(query, cb) {
-			$http.jsonp("https://en.wikipedias.org/w/api.php?action=query&format=json&prop=extracts&list=&titles=&generator=search&exsentences=1&exlimit=10&exintro=1&explaintext=1&gsrsearch=" + query)
+			$http.jsonp("https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&list=&titles=&generator=search&exsentences=1&exlimit=10&exintro=1&explaintext=1&gsrsearch=" + query)
 				.then(cb)
 				.catch(cb);
 		}	
